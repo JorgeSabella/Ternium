d9e5cfef56638ac8086d79a48dffc1611fcf8058
Paul Vazquez <pev@live.com.mx>
2019-04-29 16:34:13 -0500

    Allow cors
    Para que deje hacer delete desde la app

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
