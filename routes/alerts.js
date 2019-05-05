5c72d4619491e202769a15a15d4630f4685d42f7
Jorge Andres Sabella <jorge_andres1996@hotmail.com>
2019-05-05 17:20:27 -0500

    Added delete on alerts route

--- routes/alerts.js
+++ routes/alerts.js
@@ -7,4 +7,12 @@ router.get('/', async (req, res) => {
   res.send(alerts);
 });
 
+router.delete('/:id', async (req, res) => {
+  const alert = await Alert.findById(req.params.id);
+
+  if (!alert) return res.status(404).send('The alert with the given registration id was not found.');
+
+  res.send(alert);
+});
+
 module.exports = router;
