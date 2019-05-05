91459539b0f1abe95061e0078355aa28c85536ad
Jorge Sabella <jorge_andres1996@hotmail.com>
2019-05-05 12:57:16 -0500

    Added initial put and delete methods to users

--- routes/users.js
+++ routes/users.js
@@ -38,6 +38,37 @@ router.post('/', async (req, res) => {
   res.header('x-auth-token', token).send(_.pick(user, ['name', 'username', 'area', 'rol', 'team']));
 });
 
+router.put('/:username', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  let user = await User.findOne({ username: req.params.username });
+  if (!user) return res.status(404).send('The user with the given username was not found.');
+
+  user.set({
+    name: req.body.name,
+    username: req.body.username,
+    password: req.body.password,
+    area: req.body.area,
+    rol: req.body.rol
+  });
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  user = await user.save()
+  console.log(user);
+
+  
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['name', 'username', 'area', 'rol', 'team']));
+});
+
+router.delete('/:id', async (req, res) => {
+  const user = await User.findOneAndRemove({username: req.params.id});
+  if (!user) return res.status(404).send('The user with the given username was not found.');
+  res.send(_.pick(user, ['name', 'username', 'area', 'rol', 'team']));
+});
+
 router.get('/:id', async (req, res) => {
   const user = await User.findOne({username: req.params.id});
   if (!user) return res.status(404).send('The user with the given username was not found.');
